0a4b41a87eced0e252362f06c13a73cc018b933f
Jon Liauw <jdliauw@gmail.com>
2016-12-19 19:27:46 -0500

    Click drop-down menu

--- okcupid.py
+++ okcupid.py
@@ -1,29 +1,33 @@
 from selenium import webdriver
-import time
+import os, time
 
-zips = [ 	'28801', 	# asheville, nc
-				'94701',	# berkeley, ca
-				'80301', 	# boulder, co
-				'02108', 	# boston, ma
-				'95616',	# davis, ca
-				'80123', 	# denver, co
-				'32826',  	# orlando, fl
-				'04101', 	# portland, me
-				'97201', 	# portland, or
-				'02901',	# providence, ri
-				'94101',	# san francisco, ca
-				'90401',	# santa monica, ca
-				'98101', 	# seattle, wa
-				'98401'		# tacoma, wa
+zips = ['28801', 	# asheville, nc
+		'94701',	# berkeley, ca
+		'80301', 	# boulder, co
+		'02108', 	# boston, ma
+		'95616',	# davis, ca
+		'80123', 	# denver, co
+		'32826',  	# orlando, fl
+		'04101', 	# portland, me
+		'97201', 	# portland, or
+		'02901',	# providence, ri
+		'94101',	# san francisco, ca
+		'90401',	# santa monica, ca
+		'98101', 	# seattle, wa
+		'98401'		# tacoma, wa
 	]
 
 def login():
+	
+	# username = raw_input("username: ")
+	username = 'jdliauw@gmail.com'
+	os.system("stty -echo")
+	password = raw_input("password: ")
+	os.system("stty echo")
+
 	driver = webdriver.Firefox()
 	driver.get("http://www.okcupid.com")
 	driver.find_element_by_id('open_sign_in_button').click()
-
-	username = 'jdliauw@gmail.com'
-	password = ''
 
 	try:
 		driver.find_element_by_id('login_username').send_keys(username)
@@ -39,13 +43,8 @@ def match_page(driver):
 	time.sleep(1)
 	driver.get("https://www.okcupid.com/match")
 	
-	try:
-		driver.find_element_by_class_name('chosen-single').click()
-	except:
-		print 'failed'
+	driver.find_element_by_css_selector("[class='match-filters-in-results'] div[class='chosen-container chosen-container-single'").click()
+	print 'terminated here'
 
-	# driver.find_element_by_id('chosen-id-8pjq-1').click()
-
-
-
-login()
+if __name__ == "__main__":
+	login()
