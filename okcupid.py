41f3d76a9dfc9a42df69769e3eb2e96cc0c22936
Jon Liauw <jdliauw@gmail.com>
2016-12-21 17:10:02 -0500

    Add select by city option (selection and text entry functional, but not refreshing site.)

--- okcupid.py
+++ okcupid.py
@@ -1,30 +1,21 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import os, time
 
-zips = ['28801', 	# asheville, nc
-		'94701',	# berkeley, ca
-		'80301', 	# boulder, co
-		'02108', 	# boston, ma
-		'95616',	# davis, ca
-		'80123', 	# denver, co
-		'32826',  	# orlando, fl
-		'04101', 	# portland, me
-		'97201', 	# portland, or
-		'02901',	# providence, ri
-		'94101',	# san francisco, ca
-		'90401',	# santa monica, ca
-		'98101', 	# seattle, wa
-		'98401'		# tacoma, wa
-	]
+"""
+- Parse matches for all > 90%
+- Place cookie so don't login every time
+"""
 
-def login(driver):
+def login():
 	
 	# username = raw_input("username: ")
 	username = 'jdliauw@gmail.com'
-	# os.system("stty -echo")
-	# password = raw_input("password: ")
-	# os.system("stty echo")
+	os.system("stty -echo")
+	password = raw_input("password: ")
+	os.system("stty echo")
 
+	driver = webdriver.Firefox()
 	driver.get("http://www.okcupid.com")
 	driver.find_element_by_id('open_sign_in_button').click()
 
@@ -39,6 +30,8 @@ def login(driver):
 	except:
 		print 'Login failure, exiting\n'
 
+	return driver
+
 def sort_by_match(driver):
 	
 	# click dropdown
@@ -49,7 +42,50 @@ def sort_by_match(driver):
 	css = "[class='chosen-container chosen-container-single chosen-with-drop chosen-container-active'] ul li:nth-child(2)"
 	driver.find_element_by_css_selector(css).click()
 
+def select_city(driver, zip):
+
+	css = "span[class='filter-wrapper filter-location-locale']"
+	driver.find_element_by_css_selector(css).click()
+	
+	css = "div[class='clear-location-search'] button"
+	driver.find_element_by_css_selector(css).click()
+ 
+	xpath = "//span[text()='More options']"
+	driver.find_element_by_xpath(xpath).click()
+
+	time.sleep(1)
+	css = "div[class='okinput'] input"
+	driver.find_element_by_css_selector(css).click()
+
+	css = "input[name='lquery']"
+	driver.find_element_by_css_selector(css).send_keys(zip)
+
+def count_matches(driver):
+	
+	pass
+
+def cycle_cities(driver):
+
+	zips = ["28801", 	# asheville, nc
+		"94701",		# berkeley, ca
+		"80301", 		# boulder, co
+		"02108", 		# boston, ma
+		"95616",		# davis, ca
+		"80123", 		# denver, co
+		"32826",  		# orlando, fl
+		"04101", 		# portland, me
+		"97201", 		# portland, or
+		"02901",		# providence, ri
+		"94101",		# san francisco, ca
+		"90401",		# santa monica, ca
+		"98101", 		# seattle, wa
+		"98401"			# tacoma, wa
+	]
+
+	for zip in zips:
+		select_city(driver, zip)
+		time.sleep(1)
+
 if __name__ == "__main__":
-	driver = webdriver.Firefox()
-	login(driver)
-	sort_by_match(driver)
+	driver = login()
+	count_matches(driver)
